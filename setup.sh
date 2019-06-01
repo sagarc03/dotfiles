78a43675854c74f45f53ca974ee2e223fc01f46a
Sagar Chavan <sagar.c.03@gmail.com>
2019-06-01 16:17:07 -0700

    fix tmux files

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ mkdir ~/.vim/colors
 curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -o  ~/.vim/colors/molokai.vim
 cp vimrc ~/.vimrc
 vim +PluginInstall +qall
-cp tmux.conf ~/.tmux.conf
+cp ./tmux/conf ~/.tmux.conf
