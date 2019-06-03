63524431aceb67c880591f41d5b4e8d506f10833
Sagar Chavan <sagar.c.03@gmail.com>
2019-06-02 18:46:25 -0700

    modifies the way the files are sourced

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ mkdir ~/.vim/colors
 curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -o  ~/.vim/colors/molokai.vim
 cp vimrc ~/.vimrc
 vim +PluginInstall +qall
-cp ./tmux/conf ~/.tmux.conf
-cp ./bash/bashrc ~/.bashrc
+cp ./tmux.conf ~/.tmux.conf
+cp ./bashrc ~/.bashrc
